e11b22c89940719c9fb00cd78ac3f53acda849c8
Grzesiek1337 <gmiedzian@gmail.com>
2022-01-09 19:25:54 +0100

    added simple validate annotations

--- src/main/java/pl/gm/model/Book.java
+++ src/main/java/pl/gm/model/Book.java
@@ -4,6 +4,8 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Comparator;
 
@@ -23,10 +25,24 @@ public class Book implements Comparable<Book>, Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String isbn;
+
+    @NotBlank
+    @Size(min = 2,max=45)
     private String title;
+
+    @NotBlank
+    @Size(min = 5,max=60)
     private String author;
+
+    @NotBlank
+    @Size(min = 2,max=45)
     private String publisher;
+
+    @NotBlank
+    @Size(min = 2,max=20)
     private String type;
 
 
